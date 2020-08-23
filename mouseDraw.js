3e191c13eef34c2456aa96ef926779e5430f4f27
OkayKenji <70079754+OkayKenji@users.noreply.github.com>
2020-08-23 01:19:35 -0400

    Fixing issue  #1
    The issue has been addressed though adding "fill(0,0,0,0);" which will makes the text black.

--- mouseDraw.js
+++ mouseDraw.js
@@ -2,5 +2,6 @@ function mouseLocation() {
     stroke(0)
     rect(mouseX,mouseY,85,25);
     stroke(255,0,0,255)
+    fill(0,0,0,0);
     text("("+mouseX+","+mouseY+")" ,mouseX+15,mouseY+15);   
 }
